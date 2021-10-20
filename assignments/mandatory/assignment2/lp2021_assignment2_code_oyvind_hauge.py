7da434b325bc098a9540346a8a89ab62d03a138e
Øyvind <oyvind.hauge@evry.com>
2021-10-20 13:57:22 +0200

    LU decomposition WIP

--- assignments/mandatory/assignment2/lp2021_assignment2_code_oyvind_hauge.py
+++ assignments/mandatory/assignment2/lp2021_assignment2_code_oyvind_hauge.py
@@ -21,22 +21,56 @@ def dualize(A, b, c):
     return new_A, b_new, c_new
 
 
-def print_matrix(M):
-    for index, row in enumerate(M):
-        print(row)
+# 2. LU-decomposition
 
+def LU_decompose(A):
+    m = len(A)
+    # initialize empty lower/upper matrices
+    L = [[0 for _ in range(m)] for _ in range(m)]
+    U = [[0 for _ in range(m)] for _ in range(m)]
+    for row_idx in range(m):
+        eliminate(A, row_idx, row_idx)
+    return L, U
+
+
+# Utility functions
+
+def print_matrix(A):
+    m = len(A)
+    for row_idx in range(m):
+        row_str = ''
+        for col_idx in range(m):
+            row_str += '{:8.1f}'.format(A[row_idx][col_idx])
+        print(row_str)
 
 A = [
-    [-1, -1],
-    [-1,  1],
-    [ 1,  2]
+    [2, 0, 4, 0, -2],
+    [3, 1, 0, 1, 0],
+    [-1, 0, -1, 0, -2],
+    [0, -1, 0, 0, -6],
+    [0, 0, 1, 0, 4]
 ]
-b = [-3, -1, 2]
-c = [1, 3]
 
-A_new, b_new, c_new = dualize(A, b, c)
-print_matrix(A_new)
-print("#######")
-print_matrix(b_new)
-print("#######")
-print_matrix(c_new)
+
+def eliminate(A, pivot_row, pivot_col):
+    m, c = len(A), 0
+    for row_idx in range(pivot_row + 1, m):
+        c = A[row_idx][pivot_col] / A[pivot_row][pivot_col]
+        if c != 0:
+            for col_idx in range(m):
+                A[row_idx][col_idx] = A[row_idx][col_idx] - c * A[pivot_row][col_idx]
+
+
+#c = b[0] / a[0]
+#for i in range(len(a)):
+#    b[i] = b[i] - c * a[i]
+print_matrix(A)
+
+print("      ##################################")
+
+LU_decompose(A)
+
+print_matrix(A)
+
+#L, U = LU_decompose(A)
+#print_matrix(L)
